f9265a3cac78ca143cd0277f68219098311db8b6
Lakshan Ganesan <lakapple05@gmail.com>
2026-02-23 23:09:12 +0530

    change for the best things

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import NotFound from "./pages/NotFound";
 import CustomCursor from "./components/CustomCursor";
@@ -53,6 +54,7 @@ const App = () => {
           </Suspense>
         </BrowserRouter>
       </TooltipProvider>
+      <Analytics />
     </QueryClientProvider>
   );
 };
